cb9444b11261f8c38d9737896633eb041771bce8
Kishiannn_ <abudachan122@gmail.com>
2024-12-25 23:10:46 +0800

    Create stringManipulation.php

---
+++ stringManipulation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $greet = 'hello';   
+    echo "<br>hello <br>$greet";
+    echo '<br>hello <br>$greet<br>';
+
+    echo strlen("hello world!") . "<br>";
+    echo str_word_count("Hello world!") . "<br>";
+
+    echo strpos("Hello world!", "world!");
+
+    ?>
+</body>
+</html>
